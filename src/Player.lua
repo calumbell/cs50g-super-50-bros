6d39a135efa080d4c2d5e3037d607dbdf75ecb15
Calum Bell <calumabell@googlemail.com>
2020-07-20 20:18:19 +0100

    Player: added pickup field to store the key

--- src/Player.lua
+++ src/Player.lua
@@ -13,6 +13,9 @@ Player = Class{__includes = Entity}
 function Player:init(def)
     Entity.init(self, def)
     self.score = 0
+    self.pickups = {
+        ['key'] = false
+    }
 end
 
 function Player:update(dt)
