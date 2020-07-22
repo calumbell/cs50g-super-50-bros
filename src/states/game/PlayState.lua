b52acb2577e57bc4d02d190a449ea296835b9308
Calum Bell <calumabell@googlemail.com>
2020-07-22 19:30:01 +0100

    refactored PlayState init code into enter method to allow passing in params

--- src/states/game/PlayState.lua
+++ src/states/game/PlayState.lua
@@ -7,10 +7,10 @@
 
 PlayState = Class{__includes = BaseState}
 
-function PlayState:init()
+function PlayState:enter(params)
     self.camX = 0
     self.camY = 0
-    self.level = LevelMaker.generate(100, 10)
+    self.level = LevelMaker.generate(15, 10)
     self.tileMap = self.level.tileMap
     self.background = math.random(3)
     self.backgroundX = 0
